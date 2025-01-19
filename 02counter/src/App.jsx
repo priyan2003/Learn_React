91c4707709967feccdd44b9a8a8e6a8844e6a836
priyanshu <priyanshu.2201124cs@iiitbh.ac.in>
2025-01-19 21:53:01 +0530

    Customise Counter using hooks

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+  let [counter,setCounter] = useState(0);
+  let addValue = () =>{
+    setCounter(counter+1);
+  }
+  let removeValue = ()=>{
+    if(counter>0) setCounter(counter-1);
+  }
+  return (
+    <>
+      <h1>Kya haal hai</h1>
+      <h2>Counter is {counter}</h2>
+      <button
+      onClick = {addValue}>Add Value {counter}</button>
+      <br/>
+      <button
+      onClick={removeValue}>Decrease Value {counter}</button>
+      <p>Hence, Counter is {counter}</p>
+    </>
+  )
+}
+
+export default App
